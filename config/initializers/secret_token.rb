aaf43c8ec83ad6d382d17ae31bd448314b26e3f2
Gabriel Peal <gpeal@u.northwestern.edu>
2013-02-19 11:20:14 -0600

    Added Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NUvention::Application.config.secret_token = 'a246b747f87431fd638f6584fe7c5b3c825a669b50f16310a221393aeaa9e9e85e3a81407fa908786570f2d59655b2817b46c7a75dfde985eb76fe28dc65a7b8'
